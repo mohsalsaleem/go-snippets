7acf42c8c1123c7827a718238512c959422322cd
Mohamed Saleem <mohsal.saleem@gmail.com>
2019-03-18 02:03:56 +0530

    Added delete and contains functionality

--- datastructures/linkedlist/linkedlist.go
+++ datastructures/linkedlist/linkedlist.go
@@ -36,11 +36,76 @@ func (list *SLinkedList) Insert(value int) {
 	list.size++
 }
 
-// TODO: Implement delete and constains operations
+// Contains function checks if the given value is present in the list
+func (list *SLinkedList) Contains(value int) bool {
+	node := list.head
+	for {
+		if node != nil {
+			if node.value == value {
+				return true
+			}
+			node = node.next
+		} else {
+			break
+		}
+	}
+	return false
+}
+
+// Delete function deletes the first occurance of the node by value
+func (list *SLinkedList) Delete(value int) {
+	if list.size == 0 {
+		return
+	}
+
+	if list.head.value == value && list.size == 1 {
+		list.head = nil
+		list.tail = nil
+		list.size = 0
+		return
+	}
+
+	if list.head.value == value {
+		list.head = list.head.next
+		list.size--
+		return
+	}
+
+	curr := list.head.next
+	prev := list.head
+
+	for {
+		if curr.value == value {
+			// Case for tail
+			if curr.next == nil {
+				prev.next = nil
+				list.tail = prev
+			} else {
+				prev.next = curr.next
+			}
+			curr = nil
+			list.size--
+			break
+		} else {
+			// If the current node's next is not nil, move forward
+			// Else, break the loop
+			if curr.next != nil {
+				curr = curr.next
+				prev = prev.next
+			} else {
+				break
+			}
+		}
+	}
+}
 
 // Print function prints the list
 func (list *SLinkedList) Print() {
-	var node = list.head
+	fmt.Println("==============")
+	if list.size == 0 {
+		fmt.Println("= Empty List =")
+	}
+	node := list.head
 	for {
 		if node != nil {
 			fmt.Println(node.value)
--- datastructures/linkedlist/test/test.go
+++ datastructures/linkedlist/test/test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	sll "github.com/mohsalsaleem/go-snippets/datastructures/linkedlist"
 )
 
@@ -8,6 +10,21 @@ func main() {
 	list := sll.SLinkedList{}
 	list.Insert(1)
 	list.Insert(2)
+	list.Insert(2)
 	list.Insert(4)
+	list.Print()
+
+	fmt.Println("Does list have 2? - ", list.Contains(2))
+
+	list.Delete(2)
+	list.Delete(2)
+
+	fmt.Println("Does list have 2? - ", list.Contains(2))
+
+	list.Print()
+	fmt.Println("Does list have 1? - ", list.Contains(1))
+	list.Delete(1)
+	fmt.Println("Does list have 1? - ", list.Contains(1))
+	list.Delete(4)
 	list.Print()
 }
