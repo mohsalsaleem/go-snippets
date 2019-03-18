6e5f64822ef4655b4c6b180412f1878d41c0a14c
Mohamed Saleem <mohsal.saleem@gmail.com>
2019-03-18 15:30:52 +0530

    Implemented delete in doublylinked list

--- datastructures/doublylinkedlist/doublylinkedlist.go
+++ datastructures/doublylinkedlist/doublylinkedlist.go
@@ -24,6 +24,45 @@ func newNode(value int) Node {
 	return newNode
 }
 
+// Delete deletes the first occurrance of a node by alue
+func (list *DLinkedList) Delete(value int) {
+	node := list.head
+
+	if list.size == 1 {
+		if node.value == value {
+			node = nil
+			list.head = nil
+			list.tail = nil
+			list.size--
+			return
+		}
+	}
+
+	for {
+		if node != nil {
+			if node.value == value {
+				if node.prev == nil {
+					node.next.prev = nil
+					list.head = node.next
+				} else if node.next == nil {
+					node.prev.next = nil
+					list.tail = node.prev
+				} else {
+					node.prev.next = node.next
+					node.next.prev = node.prev
+					node = nil
+				}
+				list.size--
+				break
+			} else {
+				node = node.next
+			}
+		} else {
+			break
+		}
+	}
+}
+
 // Insert a new value to the linked list
 func (list *DLinkedList) Insert(value int) {
 	node := newNode(value)
@@ -41,6 +80,10 @@ func (list *DLinkedList) Insert(value int) {
 // Print the list
 func (list *DLinkedList) Print() {
 	node := list.head
+
+	fmt.Println("Head: ", list.head)
+	fmt.Println("Tail: ", list.tail)
+
 	for {
 		if node != nil {
 			fmt.Println(node.value)
@@ -48,6 +91,8 @@ func (list *DLinkedList) Print() {
 				break
 			}
 			node = node.next
+		} else {
+			break
 		}
 	}
 }
--- datastructures/doublylinkedlist/test/test.go
+++ datastructures/doublylinkedlist/test/test.go
@@ -1,12 +1,48 @@
 package main
 
 import (
+	"fmt"
+
 	dll "github.com/mohsalsaleem/go-snippets/datastructures/doublylinkedlist"
 )
 
 func main() {
 	list := dll.DLinkedList{}
+
+	fmt.Println("Insert 1")
 	list.Insert(1)
+
+	fmt.Println("Insert 3")
 	list.Insert(3)
+
+	fmt.Println("Insert 4")
+	list.Insert(4)
+
+	fmt.Println("Insert 5")
+	list.Insert(5)
+
+	fmt.Println("Print list")
+	list.Print()
+
+	fmt.Println("Delete 3")
+	list.Delete(3)
+	fmt.Println("Print list")
+	list.Print()
+
+	fmt.Println("Delete 1")
+	list.Delete(1)
+	fmt.Println("Print list")
+	list.Print()
+
+	fmt.Println("Delete 4")
+	list.Delete(4)
+
+	fmt.Println("Print list")
+	list.Print()
+
+	fmt.Println("Insert 1")
+	list.Insert(1)
+
+	fmt.Println("Print list")
 	list.Print()
 }
